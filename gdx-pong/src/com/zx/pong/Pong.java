167ea2412965b3626b042daee4c0e07862b1c85b
Zephilinox <Zephilinox@hotmail.co.uk>
2014-03-09 19:15:10 +0000

    Two Paddles; Two Fingers; APK added

--- gdx-pong/src/com/zx/pong/Pong.java
+++ gdx-pong/src/com/zx/pong/Pong.java
@@ -5,12 +5,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Pong implements ApplicationListener {
+	private float w;
+	private float h;
+	
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 	
@@ -23,8 +24,8 @@ public class Pong implements ApplicationListener {
 	
 	@Override
 	public void create() {	
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
+		w = Gdx.graphics.getWidth();
+		h = Gdx.graphics.getHeight();
 		
 		camera = new OrthographicCamera(w, h);
 		camera.setToOrtho(false, w, h);
@@ -36,8 +37,12 @@ public class Pong implements ApplicationListener {
 		leftPaddle = new Sprite(paddleTexture);
 		leftPaddle.setSize(32, 256);
 		leftPaddle.setOrigin(leftPaddle.getWidth()/2.f, leftPaddle.getHeight()/2.f); //No effect on translation
-		leftPaddle.setPosition(leftPaddle.getWidth() * 2, h/2.f - leftPaddle.getHeight()/2.f);		
+		leftPaddle.setPosition(leftPaddle.getWidth()*2, h/2.f - leftPaddle.getHeight()/2.f);
 		
+		rightPaddle = new Sprite(paddleTexture);
+		rightPaddle.setSize(32, 256);
+		rightPaddle.setOrigin(rightPaddle.getWidth()/2.f, rightPaddle.getHeight()/2.f);
+		rightPaddle.setPosition(w - rightPaddle.getWidth()*2, h/2.f - rightPaddle.getHeight()/2.f);
 	}
 		
 	@Override
@@ -48,11 +53,41 @@ public class Pong implements ApplicationListener {
 		
 	@Override
 	public void render() {
-		if (Gdx.input.getAccelerometerX() >= 9) {
-			leftPaddle.setY(leftPaddle.getY() - 10);
+		int leftPlayerPointer = -1;
+		int rightPlayerPointer = -1;
+		
+		if (Gdx.input.isTouched(0)) {
+			if (Gdx.input.getX(0) >= w/2.f) {
+				rightPlayerPointer = 0;
+			}
+			else {
+				leftPlayerPointer = 0;
+			}
 		}
-		else if (Gdx.input.getAccelerometerX() <= 5){
-			leftPaddle.setY(leftPaddle.getY() + 10);
+		
+		if (Gdx.input.isTouched(1)) {
+			if (Gdx.input.getX(1) >= w/2.f && rightPlayerPointer == -1) {
+				rightPlayerPointer = 1;
+			}
+			else if (Gdx.input.getX(1) < w/2.f && leftPlayerPointer == -1){
+				leftPlayerPointer = 1;
+			}
+		}
+		
+		if (leftPlayerPointer >= 0) {
+			if (h - Gdx.input.getY(leftPlayerPointer) >= leftPaddle.getY() + leftPaddle.getHeight()/2.f) {
+				leftPaddle.setY(leftPaddle.getY() + 7);
+			} else {
+				leftPaddle.setY(leftPaddle.getY() - 7);
+			}
+		}
+		
+		if (rightPlayerPointer >= 0) {
+			if (h - Gdx.input.getY(rightPlayerPointer) >= rightPaddle.getY() + rightPaddle.getHeight()/2.f) {
+				rightPaddle.setY(rightPaddle.getY() + 7);
+			} else {
+				rightPaddle.setY(rightPaddle.getY() - 7);
+			}
 		}
 		
 		Gdx.gl.glClearColor(
@@ -65,6 +100,7 @@ public class Pong implements ApplicationListener {
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		leftPaddle.draw(batch);
+		rightPaddle.draw(batch);
 		batch.end();
 	}
 		
