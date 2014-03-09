1530eb2dd6cb84dd62812313bbefd1c7555af8aa
Zephilinox <Zephilinox@hotmail.co.uk>
2014-03-09 19:45:26 +0000

    Added ball; Added line seperator

--- gdx-pong/src/com/zx/pong/Pong.java
+++ gdx-pong/src/com/zx/pong/Pong.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Pong implements ApplicationListener {
 	private float w;
@@ -22,6 +25,10 @@ public class Pong implements ApplicationListener {
 	private Sprite rightPaddle;
 	private Sprite ball;
 	
+	private boolean ballMovingLeft;
+	
+	private ShapeRenderer shapeRenderer;
+	
 	@Override
 	public void create() {	
 		w = Gdx.graphics.getWidth();
@@ -30,9 +37,11 @@ public class Pong implements ApplicationListener {
 		camera = new OrthographicCamera(w, h);
 		camera.setToOrtho(false, w, h);
 		batch = new SpriteBatch();
+		shapeRenderer = new ShapeRenderer();
 		
 		paddleTexture = new Texture(Gdx.files.internal("square.png"));
 		//paddleTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		ballTexture = new Texture(Gdx.files.internal("square.png"));
 		
 		leftPaddle = new Sprite(paddleTexture);
 		leftPaddle.setSize(32, 256);
@@ -43,6 +52,12 @@ public class Pong implements ApplicationListener {
 		rightPaddle.setSize(32, 256);
 		rightPaddle.setOrigin(rightPaddle.getWidth()/2.f, rightPaddle.getHeight()/2.f);
 		rightPaddle.setPosition(w - rightPaddle.getWidth()*2, h/2.f - rightPaddle.getHeight()/2.f);
+		
+		ball = new Sprite(ballTexture);
+		ball.setOrigin(ball.getWidth()/2.f, ball.getHeight()/2.f);
+		ball.setPosition(w/2.f + ball.getWidth()/2.f, h/2.f + ball.getHeight()/2.f);
+		
+		ballMovingLeft = true;
 	}
 		
 	@Override
@@ -55,6 +70,26 @@ public class Pong implements ApplicationListener {
 	public void render() {
 		int leftPlayerPointer = -1;
 		int rightPlayerPointer = -1;
+		
+		Rectangle leftPaddleRect = new Rectangle(leftPaddle.getX(), leftPaddle.getY(), leftPaddle.getWidth(), leftPaddle.getHeight());
+		Rectangle rightPaddleRect = new Rectangle(rightPaddle.getX(), rightPaddle.getY(), rightPaddle.getWidth(), rightPaddle.getHeight());
+		Rectangle ballRect = new Rectangle(ball.getX(), ball.getY(), ball.getWidth(), ball.getHeight());
+		
+		if (ballRect.overlaps(leftPaddleRect) || ballRect.overlaps(rightPaddleRect)) {
+			ballMovingLeft = !ballMovingLeft;
+		}
+		
+		if (ballMovingLeft) {
+			ball.setX(ball.getX() - 10);
+		} else {
+			ball.setX(ball.getX() + 10);
+		}
+		
+		if (ball.getX() + ball.getWidth() <= 0 || ball.getX() >= w) {
+			ball.setX(w/2.f + ball.getWidth()/2.f);
+			ball.setY(h/2.f + ball.getHeight()/2.f);
+			ballMovingLeft = !ballMovingLeft;
+		}
 		
 		if (Gdx.input.isTouched(0)) {
 			if (Gdx.input.getX(0) >= w/2.f) {
@@ -101,8 +136,14 @@ public class Pong implements ApplicationListener {
 				255.f / 255.f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
+		shapeRenderer.begin(ShapeType.Line);
+		shapeRenderer.setColor(100.f / 255.f, 100.f / 255.f, 100.f / 255.f, 1);
+		shapeRenderer.line(w/2.f, 0, w/2.f, h);
+		shapeRenderer.end();
+		
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
+		ball.draw(batch);
 		leftPaddle.draw(batch);
 		rightPaddle.draw(batch);
 		batch.end();
